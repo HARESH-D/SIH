8a10f0788fac32baf933b686f1361abf2a716d02
haresh <hareshdinkar@gmail.com>
2022-03-27 00:07:30 +0530

    final_model

---
+++ SIH.py
@@ -0,0 +1,22 @@
+body {
+    color: #fff;
+    background-color: #4F8BF9;
+}
+
+.stButton>button {
+
+    color: #F8BF9;
+    border-radius: 0%;
+    height: 3em;
+    width: 4em;
+    position: absolute;
+    left: 720px;
+    top: -64px;
+    bottom:2px;
+    right: 40px;
+
+}
+
+.stTextInput>div>div>input {
+    color: #4F8BF9;
+}
